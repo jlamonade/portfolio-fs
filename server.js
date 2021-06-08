dcd229b76fa39b95e20650ba4c66ee104ccd934c
Jason Lam <nyjasonlam@gmail.com>
2021-06-08 19:43:34 -0400

    added server and mongoose db connection

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const logger = require('morgan')
+const mongooose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+const controller = require('./controller')
+
+// middlewares
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
+})
+
+mongooose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/portfolio', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
